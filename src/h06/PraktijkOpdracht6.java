09f3aff2caa748bbc7151d9f5cc7bbbb02694af2
Jaydenian99 <jwong@roc-dev.com>
2021-01-20 10:03:01 +0100

    Hoofdstuk 6

--- src/h06/PraktijkOpdracht6.java
+++ src/h06/PraktijkOpdracht6.java
@@ -9,47 +9,24 @@ public class PraktijkOpdracht6 extends Applet {
     double FiveNine;
     double SixThree;
     double SixNine;
-    double UitkomstBijElkaarOpgeteld;
+
+    //Uitkomstgemiddeld om te vermenigvuldigen
     double UitKomstGemiddeld;
 
-    //Int to convert
+    //To convert for answer
     double ToConvert1;
-
-    //Double to convert back
-    double ToConvertBack;
-
-    //Int to divide
-    int Divide;
-
-    //Int to multiply and divide
-    int MultiVide;
-
-    //One more
-    double AverageTimesTen;
-
-    //Zeros
-    int Zero;
-
-    //Voor hele getal
-    double random;
 
     public void init() {
 
         setSize(500, 500);
         setBackground(Color.darkGray);
-        FiveNine = 5.9;
+        FiveNine = 5.8;
         SixThree = 6.3;
         SixNine = 6.9;
-        Divide = 3;
-        Zero = 0;
-        UitkomstBijElkaarOpgeteld = FiveNine + SixThree + SixNine;
-        UitKomstGemiddeld = UitkomstBijElkaarOpgeteld / Divide;
-        MultiVide = 10;
-        AverageTimesTen = UitKomstGemiddeld * MultiVide;
-        ToConvert1 = AverageTimesTen + Zero;
-        ToConvertBack = ToConvert1 / MultiVide;
-        random = 0.066666666666667;
-        ToConvertBack = UitKomstGemiddeld - random;
+
+        UitKomstGemiddeld = (FiveNine + SixThree + SixNine) / 3;
+        ToConvert1 = Math.round(UitKomstGemiddeld * 10 ) / 10.0;
+
 
 
 
@@ -60,7 +37,7 @@ public class PraktijkOpdracht6 extends Applet {
         g.setColor(Color.white);
         g.drawString("Een cursist heeft drie cijfers behaald: 5,9; 6.3; 6,9.", 50, 30);
         g.drawString("Hij wilt weten wat zijn gemiddelde cijfer is. ", 50, 50);
-        g.drawString("Het gemiddelde is: " + ToConvertBack, 50, 70);
+        g.drawString("Het gemiddelde is: " + ToConvert1, 50, 70);
 
 
     }
