3d3ee26f5e7b963f2efb48ef239045a7e9d656a5
Jaydenian99 <jwong@roc-dev.com>
2021-02-03 22:46:23 +0100

    Hoofdstuk 11

---
+++ src/h11/Opdracht119.java
@@ -0,0 +1,149 @@
+package h11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht119 extends Applet {
+
+    public void init() {
+
+        setSize(500, 500);
+        setBackground(Color.lightGray);
+
+    }
+
+    public void paint(Graphics g) {
+
+        int y = 50;
+        int x = 50;
+        int breedte = 25;
+        int hoogte = 25;
+
+        //First row
+        for (int kolom = 0; kolom < 8; kolom++) {
+
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+            }
+            x += breedte;
+        }
+
+        //Second row
+        x = 50;
+        y += hoogte;
+
+        for (int kolom = 0; kolom < 8; kolom++) {
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+
+            }
+            x += breedte;
+        }
+
+        //Third row
+        x = 50;
+        y += hoogte;
+        for (int kolom = 0; kolom < 8; kolom++) {
+
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+            }
+            x += breedte;
+
+        }
+
+        //Fourth row
+        x = 50;
+        y += hoogte;
+
+        for (int kolom = 0; kolom < 8; kolom++) {
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+
+            }
+            x += breedte;
+        }
+
+        //Fifth row
+        x = 50;
+        y += hoogte;
+        for (int kolom = 0; kolom < 8; kolom++) {
+
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+            }
+            x += breedte;
+        }
+
+        //Sixth row
+        x = 50;
+        y += hoogte;
+
+        for (int kolom = 0; kolom < 8; kolom++) {
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+
+            }
+            x += breedte;
+        }
+
+        //Seventh Row
+        x = 50;
+        y += hoogte;
+        for (int kolom = 0; kolom < 8; kolom++) {
+
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+            }
+            x += breedte;
+        }
+
+        //Eight Row
+        x = 50;
+        y += hoogte;
+
+        for (int kolom = 0; kolom < 8; kolom++) {
+            if (kolom == 0 || kolom == 2 || kolom == 4 || kolom == 6) {
+                g.setColor(Color.white);
+                g.fillRect(x, y, breedte, hoogte);
+            } else {
+                g.setColor(Color.black);
+                g.fillRect(x, y, breedte, hoogte);
+
+            }
+            x += breedte;
+        }
+
+        g.setColor(Color.black);
+        g.drawRect(50, 50, 200, 200);
+    }
+
+}
