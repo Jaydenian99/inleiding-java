70d56a0a13024d14398e384a52c9df1e40fbe14a
Jaydenian99 <jwong@roc-dev.com>
2021-02-03 17:46:49 +0100

    Hoofdstuk 11

--- src/h11/Opdracht111.java
+++ src/h11/Opdracht111.java
@@ -15,10 +15,10 @@ public class Opdracht111 extends Applet {
     public void paint(Graphics g) {
 
         g.setColor(Color.magenta);
-        int x = 20;
+        int xAxis = 20;
         for (int i = 0; i < 10; i++) {
-            g.drawLine(x, 20 , x, 400);
-            x += 40;
+            g.drawLine(xAxis, 20 , xAxis, 400);
+            xAxis += 40;
         }
 
     }
